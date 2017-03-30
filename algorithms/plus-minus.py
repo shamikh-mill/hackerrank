ea278882e232e74486a99295b38ef56c7b6083d5
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-03-30 13:11:53 -0400

    plus minus

--- algorithms/plus-minus.py
+++ algorithms/plus-minus.py
@@ -5,3 +5,14 @@ import sys
 
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+
+numpos, numneg, numzero = 0, 0, 0
+for x in arr: 
+	if x == 0: numzero+=1
+	if x > 0: numpos+=1
+	if x < 0: numneg+=1
+
+print (numpos/n)
+print (numneg/n)
+print (numzero/n)
