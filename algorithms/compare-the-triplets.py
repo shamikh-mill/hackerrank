3777deeef2940a73c31e6783ad0686642d59dfc8
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-03-30 11:47:32 -0400

    another one

---
+++ algorithms/compare-the-triplets.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+
+a0,a1,a2 = input().strip().split(' ')
+a0,a1,a2 = [int(a0),int(a1),int(a2)]
+b0,b1,b2 = input().strip().split(' ')
+b0,b1,b2 = [int(b0),int(b1),int(b2)]
+
+a, b = 0, 0 
+for ai, bi in zip((a0,a1,a2), (b0,b1,b2)): 
+	if (ai > bi): 
+		a += 1 
+	if (bi > ai): 
+		b+= 1
+
+print (a, b)
---
+++ algorithms/simple-array-sum.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+import sys
+
+def sum(): 
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+return sum(array) 
